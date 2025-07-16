a4c2ea8f310c3e4bda5f078a5fa9594963aa2447
SHEMA IRADUKUNDA Daniel <danieliradukunda00@gmail.com>
2025-07-16 13:58:22 +0200

    adding button component to index interface

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -1,6 +1,1 @@
-export interface ButtonProps {
-  title: string;
-  size?: "small" | "medium" | "large";
-  shape?: "rounded-sm" | "rounded-md" | "rounded-full" | "rounded-lg";
-  styles?: string;
-}
+import "@/interfaces/landing";
